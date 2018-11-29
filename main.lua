4d28b3c25d0f51034653a8931e0f9ed046ed9dce
William Bouillon <musique88181@gmail.com>
2018-11-28 19:24:25 -0500

    oiqdw

--- main.lua
+++ main.lua
@@ -1,19 +1,27 @@
 require "utils"
 
 function love.load()
+  love.window.setMode(1280,720)
   state = "idle"
-  rectangles = {}
+  rectangles = {rectangleO:newPassive(-1000,-1000,1010,2280,"normal"),
+                rectangleO:newPassive(0,710,1280,1010,"normal"),
+                rectangleO:newPassive(1270,-1000,1010,2280,"normal")}
   activeRectangle=nil
+  rAmt=50
+  love.filesystem.write("map", "")
+  activeType="normal"
 end
 
 function love.update(dt)
   mousePos = vectorO:new(love.mouse.getX(), love.mouse.getY())
-  if not activeRectangle == nil then
+  if activeRectangle ~= nil then
     activeRectangle:update()
   end
   if #rectangles>0 then
-    for i=1, #rectangles do
-      rectangles[i]:update()
+    if rectangles[i] ~= nil then
+      for i=1, #rectangles do
+        rectangles[i]:update()
+      end
     end
   end
 end
@@ -21,7 +29,7 @@ end
 function love.mousepressed(x,y,btn)
   if btn==1 then
     if state == "idle" then
-      activeRectangle = rectangleO:new(x,y,0,0)
+      activeRectangle = rectangleO:new(x,y,0,0,activeType)
       state = "drawing"
     else
       activeRectangle:setPassive()
@@ -32,22 +40,88 @@ function love.mousepressed(x,y,btn)
 end
 
 function love.draw()
-  debug()
-  if not activeRectangle == nil then
+  if activeRectangle ~= nil then
     activeRectangle:draw()
   end
   if #rectangles>0 then
     for i=1, #rectangles do
-      rectangles[i]:draw()
+      if rectangles[i] ~= nil then
+        rectangles[i]:draw()
+      end
     end
   end
+  debug()
 end
 
-function love.keypressed(btn, isRepeat)
+function love.keypressed(btn,scanCode,isRepeat)
   if btn == "escape" then
     love.event.quit()
   end
   if btn == "return" then
-    love.filesystem.append("test", "bie")
+    for i=1, #rectangles do
+      love.filesystem.append("map", "rectangleO:new("..
+      rectangles[i].x..","..
+      rectangles[i].y..","..
+      rectangles[i].w..","..
+      rectangles[i].h..",\""..
+      rectangles[i].type.."\")\n")
+    end
+  end
+  if btn =="f" then
+    if state == "idle" then
+      activeRectangle = rectangleO:new(mousePos.x,mousePos.y,0,0,activeType)
+      state = "drawing"
+    else
+      activeRectangle:setPassive()
+      state = "idle"
+      activeRectangle = nil
+    end
+  end
+  if btn == "1" then
+    activeType = "normal"
+  elseif btn == "2" then
+    activeType = "bouncing"
+  end
+  if btn == "x" then
+    love.load()
+  end
+  if btn == "z" then
+    if #rectangles > 3 then
+      rectangles[#rectangles] = nil
+    end
+  end
+  --moving mouseCursor
+  if btn == "up" and love.keyboard.isDown("w") then
+    love.mouse.setY(mousePos.y-10)
+  elseif btn == "down" and love.keyboard.isDown("w") then
+    love.mouse.setY(mousePos.y+10)
+  elseif btn == "left" and love.keyboard.isDown("w") then
+    love.mouse.setX(mousePos.x-10)
+  elseif btn == "right" and love.keyboard.isDown("w") then
+    love.mouse.setX(mousePos.x+10)
+  elseif btn == "up" and love.keyboard.isDown("e") then
+    love.mouse.setY(mousePos.y-100)
+  elseif btn == "down" and love.keyboard.isDown("e") then
+    love.mouse.setY(mousePos.y+100)
+  elseif btn == "left" and love.keyboard.isDown("e") then
+    love.mouse.setX(mousePos.x-100)
+  elseif btn == "right" and love.keyboard.isDown("e") then
+    love.mouse.setX(mousePos.x+100)
+  elseif btn == "up" and love.keyboard.isDown("r") then
+    love.mouse.setY(mousePos.y-rAmt)
+  elseif btn == "down" and love.keyboard.isDown("r") then
+    love.mouse.setY(mousePos.y+rAmt)
+  elseif btn == "left" and love.keyboard.isDown("r") then
+    love.mouse.setX(mousePos.x-rAmt)
+  elseif btn == "right" and love.keyboard.isDown("r") then
+    love.mouse.setX(mousePos.x+rAmt)
+  elseif btn == "up" then
+    love.mouse.setY(mousePos.y-1)
+  elseif btn == "down" then
+    love.mouse.setY(mousePos.y+1)
+  elseif btn == "left" then
+    love.mouse.setX(mousePos.x-1)
+  elseif btn == "right" then
+    love.mouse.setX(mousePos.x+1)
   end
 end
--- utils.lua
+++ utils.lua
@@ -1,4 +1,5 @@
 function debug()
+  love.graphics.setColor(1,1,1)
   love.graphics.print(mousePos.x,0,0)
   love.graphics.print(mousePos.y,100,0)
   love.graphics.print(state,0,12)
@@ -20,8 +21,13 @@ end
 rectangleO = {}
 rectangleO.__index = rectangleO
 
-function rectangleO:new(x,y,w,h)
-  local newRectangle = {x=x, y=y, w=w, h=h, state="active"}
+function rectangleO:new(x,y,w,h,type)
+  local newRectangle = {x=x, y=y, w=w, h=h,type=type or "normal", state="active"}
+  setmetatable(newRectangle,rectangleO)
+  return newRectangle
+end
+function rectangleO:newPassive(x,y,w,h,type)
+  local newRectangle = {x=x, y=y, w=w, h=h,type=type or "normal", state="passive"}
   setmetatable(newRectangle,rectangleO)
   return newRectangle
 end
@@ -36,9 +42,26 @@ end
 function rectangleO:setPassive()
   self.state="passive"
   table.insert(rectangles,self)
+  self:normalizeRectangle()
 end
 
 function rectangleO:draw()
+  if self.type == "normal" then
+    love.graphics.setColor(1,0,0)
+  elseif self.type == "bouncing" then
+    love.graphics.setColor(0.5,0,0)
+  end
   love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+end
+
+function rectangleO:normalizeRectangle()
+  if self.w < 0 then
+    self.x = self.x + self.w
+    self.w = -self.w
+  end
+  if self.h < 0 then
+    self.y = self.y + self.h
+    self.h = -self.h
+  end
 end
 --rectangle/
